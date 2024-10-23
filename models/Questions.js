1f90675e584cbb934ab1377595396bf61e7729eb
Rui Gaspar <ruigaspar03@hotmail.com>
2024-10-23 14:00:28 +0200

    get answers in model

--- models/Questions.js
+++ models/Questions.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const questionSchema = new mongoose.Schema({
   question: String,
   difficulty: String,
+  answer: String,
 });
 
 const Question = mongoose.model("Question", questionSchema);
