1559270c54b606603434154928b411b57080bf66
Rui Gaspar <ruigaspar03@hotmail.com>
2024-10-25 17:29:18 +0200

    add timeoutExpired to model

--- models/Room.js
+++ models/Room.js
@@ -20,6 +20,7 @@ const RoomSchema = new mongoose.Schema({
   currentRound: { type: Number, default: 1 },
   gameState: { type: String, default: "lobby" },
   questions: { type: Array, default: [] },
+  timeoutExpired: { type: Boolean, default: false },
 });
 
 const Room = mongoose.model("Room", RoomSchema);
